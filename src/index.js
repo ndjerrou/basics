587cfc75260d001a9930c6ad2816cf2c226c7f73
ndjerrou <n.djerrou@gmail.com>
2024-01-08 14:25:43 +0100

    First comp mounted

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// React - ReactDOM
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import { createRoot } from 'react-dom/client';
+
+import HomePage from './routing/HomePage';
+
+const root = createRoot(document.querySelector('#root'));
+
+const firstName = 'Nissim';
+const age = 33.5;
+const gender = 'F';
+const isM = gender === 'M' ? true : false;
+const bankAccount = null;
+const undef = undefined;
+const me = {
+  name: 'Nissim',
+  age: 33,
+};
+const hobbies = ['Roller', 'Foot', 'Escalade'];
+
+// display multiple types of values inside jsx and check the web app
+const template = (
+  <>
+    <h1>Hobbies de {firstName}</h1>
+    <p>age : {age}</p>
+    <p>Sexe masculin ? {isM}</p>
+    <p>Bank Account : {bankAccount}</p>
+    <p>Do u display undefined ? {undef}</p>
+    {/* <p>Qui suis-je ? {me}</p> */}
+    {/* <p>Mes superbes hobbies : {hobbies}</p> */}
+    {/* <ul>
+      <li>Foot</li>
+      <li>Roller</li>
+      <li>Escalade</li>
+      <li>Basket</li>
+    </ul> */}
+    {/* Display my hobbies programmatically using the array*/}
+    <ul>
+      {hobbies.map(function (hobby) {
+        return <li>{hobby}</li>;
+      })}
+    </ul>
+  </>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<HomePage />);
---
+++ src/routing/HomePage.js
@@ -0,0 +1,5 @@
+const HomePage = () => {
+  return <div>Kikoo from HomePage</div>;
+};
+
+export default HomePage;
